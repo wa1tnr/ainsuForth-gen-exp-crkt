df611bd54fd3b437e78207524813059b5fcaaa60
wa1tnr <wa1tnr@users.noreply.github.com>
2018-05-30 17:57:07 +0000

    new file:   accept.cpp 	new file:   accept.h 	new file:   allot.cpp 	new file:   allot.h 	new file:   cblink.cpp 	new file:   cblink.h 	new file:   char.cpp 	new file:   char.h 	new file:   colon_semi.cpp 	new file:   colon_semi.h 	new file:   constant.cpp 	new file:   constant.h 	new file:   dict_entries.cpp 	new file:   dict_entries.h 	new file:   do_loop.cpp 	new file:   do_loop.h 	new file:   dump.cpp 	new file:   dump.h 	new file:   help.cpp 	new file:   help.h 	new file:   iasmd.cpp 	new file:   iasmd.h 	new file:   max_min.cpp 	new file:   max_min.h 	new file:   stack_ops.cpp 	new file:   stack_ops.h 	new file:   store_fetch.cpp 	new file:   store_fetch.h 	new file:   xttoname.cpp 	new file:   xttoname.h

---
+++ src/dict/dict_entries.h
@@ -0,0 +1,48 @@
+// Sat Jul 29 18:14:02 UTC 2017
+// 4735-b0b-01-
+
+// count word evaluate : ; c, here 
+
+extern const char invert_str[]; // = "invert";
+extern void _invert(void);
+
+extern const char then_str[]; // = "then";
+extern void _then(void);
+
+extern const char if_str[]; // = "if";
+extern void _if(void);
+
+extern const char two_star_str[]; // = "2*";
+extern void _two_star(void);
+
+extern const char count_str[]; // = "count";
+extern void _count(void);
+
+extern const char word_str[]; // = "word";
+extern void _word(void);
+
+extern const char evaluate_str[]; // = "evaluate";
+extern void _evaluate(void);
+
+extern const char colon_str[]; // = ":";
+extern void _colon(void);
+
+extern const char semicolon_str[]; // = ";";
+extern void _semicolon(void);
+
+extern const char here_str[]; // = "here";
+extern void _here(void);
+
+extern const char see_str[]; // = "see";
+extern void _see(void);
+
+extern const char pinWrite_str[]; // = "pinWrite";
+extern void _pinWrite(void);
+
+extern const char pinMode_str[]; // = "pinMode";
+extern void _pinMode(void);
+
+extern const char pinRead_str[]; // = "pinRead";
+extern void _pinRead(void);
+
+
